157faf3d1b6c51ee5885fcd504d8a079d81977e0
sim2kid <sim2kid@users.noreply.github.com>
2021-09-16 02:39:11 -0500

    Add the logic for the demo script

--- Assets/Scripts/Demo/DemoTextureConversion.cs
+++ Assets/Scripts/Demo/DemoTextureConversion.cs
@@ -16,7 +16,7 @@ public class DemoTextureConversion : MonoBehaviour
     {
         if (colors.Length > MAX_COLORS) 
         {
-            Debug.LogWarning("You can only add upto 3 colors for a conversion!");
+            Debug.LogWarning($"You can only add upto {MAX_COLORS} colors for a conversion!");
             Array.Resize(ref colors, MAX_COLORS);
         }
     }
@@ -24,12 +24,14 @@ public class DemoTextureConversion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TextureToModify = (Texture2D)this.GetComponent<Material>().mainTexture;
-        // Set the '_MainTex' to our new texture
-        this.GetComponent<Material>().SetTexture("_MainTex", ModifyTexture(TextureToModify, colors, OverlayTexture));
+        // Get the pattern texture from the material
+        TextureToModify = (Texture2D)this.GetComponent<Renderer>().material.mainTexture;
+
+        // Set the main texture to our new texture
+        this.GetComponent<Renderer>().material.mainTexture = ModifyTexture(TextureToModify, colors, OverlayTexture);
     }
 
-    static Texture ModifyTexture(Texture2D toModify, Color[] newColors, Texture2D textureDetails = null) 
+    static Texture2D ModifyTexture(Texture2D toModify, Color[] newColors, Texture2D textureDetails = null) 
     {
         // Warning if the modify and details texture are different sizes. Cropping will happen since this is a pixel perfect operation.
         if (textureDetails != null)
@@ -44,16 +46,32 @@ public class DemoTextureConversion : MonoBehaviour
         // The new texture's output so we aren't overriding a texture.
         Texture2D output = new Texture2D(toModify.width, toModify.height);
 
+        // Loop through each pixel and set it's new color based on the old ones.
         for (int width = 0; width < output.width; width++) 
         {
             for (int height = 0; height < output.height; height++) 
             {
-                //TODO: Actual conversions go here
+                Vector4 patternColor = toModify.GetPixel(width, height);
+                //Only get this color if the texture isn't null or out of bounds
+                Vector4 detailColor = (textureDetails != null && textureDetails.width > width && textureDetails.height > height ?
+                    textureDetails.GetPixel(width, height) : Color.clear);
+
+                // We multiply each channel with the associated color
+                Vector4 r = (newColors.Length >= 1 ? newColors[0] : Color.clear) * patternColor.x;
+                Vector4 g = (newColors.Length >= 2 ? newColors[1] : Color.clear) * patternColor.y;
+                Vector4 b = (newColors.Length >= 3 ? newColors[2] : Color.clear) * patternColor.z;
+                Vector4 a = (newColors.Length >= 4 ? newColors[3] : Color.clear) * patternColor.w;
+
+                // We add those colors together, then we apply the coat texture over that.
+                Vector4 outColor = (r + g + b + a) * (1 - (((detailColor.x + detailColor.y + detailColor.z) / 3) * detailColor.w));
+
+                output.SetPixel(width, height, outColor);
             }
         }
+        // Apply our changes!
+        output.Apply();
 
-        return (Texture)output;
+        return output;
     }
-
     
 }
