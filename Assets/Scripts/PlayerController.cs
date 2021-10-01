15ab52806487cf6f6d12e41c520a9af4cc57e841
Max Garay <382535@colum.edu>
2021-10-01 11:08:28 -0500

    adds basic movement and jumping
    TODO: + smooth out movement + fix jump height inconsistencies

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10.0f;
+    [SerializeField] float gravityForce = 1.0f;
+    [SerializeField] float jumpHeight = 2.0f;
+    CharacterController characterController;
+    Vector2 moveValue;
+    Vector3 moveTo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+
+        Debug.Log($"Current movement value is: ({moveValue.x},{moveValue.y})");
+
+        moveTo = new Vector3(moveValue.x, 0, moveValue.y);
+    }
+
+    void OnJump()
+    {
+        Debug.Log("Jump input received.");
+
+        HandleJumping();
+    }
+
+    void HandleJumping()
+    {
+        if (characterController.isGrounded)
+        {
+            moveTo.y = jumpHeight;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        moveTo.y -= gravityForce * Time.fixedDeltaTime;
+        characterController.Move(moveTo.normalized * Time.fixedDeltaTime * moveSpeed);
+    }
+}
