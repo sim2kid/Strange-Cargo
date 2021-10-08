d641fedcfbd4bcfee62bfa0e5e0d9269c0325890
Max Garay <382535@colum.edu>
2021-10-08 10:59:09 -0500

    updates HandleGravity logic to behave more realistically and not interfere with player movement
    + updates HandleJumping logic to behave more realistically & cooperate with new HandleGravity logic
    + organizes references into distinct categories for readability
    
    TODO: fix movement to cooperate with mouselook so that movement direction automatically updates in real time to correspond with current rotation, even while a movement key is being held down

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //references for handling movement
     [SerializeField] float moveSpeed = 10.0f;
-    [SerializeField] float gravityForce = 1.0f;
-    [SerializeField] float jumpHeight = 2.0f;
     CharacterController characterController;
     Vector2 moveValue;
     Vector3 moveTo;
+
+    //references for handling gravity
+    Vector3 velocity;
+    [SerializeField] float gravityForce = 1.0f;
+    [SerializeField] Transform feet;
+    [SerializeField] float groundCheckDistance;
+    [SerializeField] LayerMask groundMask;
+    [Tooltip("Reset the player's downward velocity to this number when landing from a fall or jump (float).")]
+    [SerializeField] float groundCheckVelocityReset = -2.0f;
+    bool isOnGround;
+    float jumpForceFormula;
+
+    //references for handling jumping
+    [SerializeField] float jumpHeight = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        jumpForceFormula = Mathf.Sqrt(jumpHeight * -2.0f * -gravityForce);
     }
 
     void OnMove(InputValue value)
@@ -31,25 +46,31 @@ public class PlayerController : MonoBehaviour
 
     void HandleJumping()
     {
-        if (characterController.isGrounded)
+        if (isOnGround)
         {
-            moveTo.y = jumpHeight;
+            velocity.y = jumpForceFormula;
         }
     }
 
     void FixedUpdate()
     {
-        //HandleGravity();
+        HandleGravity();
         HandleMovement();
     }
 
     void HandleMovement()
     {
-        characterController.Move(moveTo.normalized * Time.fixedDeltaTime * moveSpeed);
+        characterController.Move(moveTo.normalized * Time.fixedDeltaTime * moveSpeed);//gameObject.transform.forward * moveValue.normalized * Time.fixedDeltaTime * moveSpeed);
     }
 
     void HandleGravity()
     {
-        moveTo.y -= gravityForce * Time.fixedDeltaTime; //TODO: Mend or outright replace gravity logic; current logic causes player to gradually slow to a crawl the longer a movement key is held
+        isOnGround = Physics.CheckSphere(feet.position, groundCheckDistance, groundMask);
+        if(isOnGround && velocity.y < 0)
+        {
+            velocity.y = groundCheckVelocityReset;
+        }
+        velocity.y -= gravityForce * Time.fixedDeltaTime;
+        characterController.Move(velocity * Time.fixedDeltaTime);
     }
 }
