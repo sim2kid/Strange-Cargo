c34a813be274ee53f7408c56e4e25ad4270c64d5
sim2kid <sim2kid@users.noreply.github.com>
2021-10-18 20:58:09 -0500

    Properly imported the Dialogue System

--- Assets/DialogueSystem/GeneratedCode/Demo_DialogueCode.cs
+++ Assets/DialogueSystem/GeneratedCode/Demo_DialogueCode.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 
 namespace DialogueSystem.Code
 {
-    public class New_Dialogue_DialogueCode : IDialogueCode
+    public class Demo_DialogueCode : IDialogueCode
     {
         private Dictionary<string, IDialogueCode.EventDelegate> eventFunctions = new Dictionary<string, IDialogueCode.EventDelegate>();
         private Dictionary<string, IDialogueCode.ConditionDelegate> conditionChecks = new Dictionary<string, IDialogueCode.ConditionDelegate>();
@@ -19,7 +19,7 @@ namespace DialogueSystem.Code
         public string GetVariable(string variableName) {
             return this.GetType().GetField(variableName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).GetValue(this).ToString(); 
         }
-        public New_Dialogue_DialogueCode() 
+        public Demo_DialogueCode() 
         {
             Start();
         }
@@ -34,48 +34,48 @@ string name = "Becky Sue";
         public void Start()
         {
             // Setup //
-            eventFunctions.Add("New_Dialogue_538836df8f2f45c084d230196dcaf5e9",New_Dialogue_538836df8f2f45c084d230196dcaf5e9);
-            eventFunctions.Add("New_Dialogue_30b2fb78ba854d0986b2329052b9ecc4",New_Dialogue_30b2fb78ba854d0986b2329052b9ecc4);
-            eventFunctions.Add("New_Dialogue_46a607dd382c49d2a8c319de735e8157",New_Dialogue_46a607dd382c49d2a8c319de735e8157);
-            dialogueChecks.Add("New_Dialogue_1105168d7cf24844bf025efcc454013f_9c8aec032182475b98a2d7ef96b5887c",New_Dialogue_1105168d7cf24844bf025efcc454013f_9c8aec032182475b98a2d7ef96b5887c);
-            dialogueChecks.Add("New_Dialogue_1105168d7cf24844bf025efcc454013f_2e82160d18ce4a8b812ab874a3fbb742",New_Dialogue_1105168d7cf24844bf025efcc454013f_2e82160d18ce4a8b812ab874a3fbb742);
-            dialogueChecks.Add("New_Dialogue_1105168d7cf24844bf025efcc454013f_37d9ac168f6f4307895dd5f569baf054",New_Dialogue_1105168d7cf24844bf025efcc454013f_37d9ac168f6f4307895dd5f569baf054);
-            conditionChecks.Add("New_Dialogue_fd248e7fbb8b43a0bce1aab11f7ae84f",New_Dialogue_fd248e7fbb8b43a0bce1aab11f7ae84f);
-            eventFunctions.Add("New_Dialogue_7b28363f37d44e84a6b37b83252ff565",New_Dialogue_7b28363f37d44e84a6b37b83252ff565);
-            eventFunctions.Add("New_Dialogue_465747a1391f4eefac5d2a1344edd8e5",New_Dialogue_465747a1391f4eefac5d2a1344edd8e5);
-            dialogueChecks.Add("New_Dialogue_27776fa84a1644d18680558c02f485c3_8c5a554f8fb74b0c88efa338653a0a9a",New_Dialogue_27776fa84a1644d18680558c02f485c3_8c5a554f8fb74b0c88efa338653a0a9a);
-            dialogueChecks.Add("New_Dialogue_27776fa84a1644d18680558c02f485c3_342e6c8780eb44cb90168651300a2253",New_Dialogue_27776fa84a1644d18680558c02f485c3_342e6c8780eb44cb90168651300a2253);
+            eventFunctions.Add("Demo_538836df8f2f45c084d230196dcaf5e9",Demo_538836df8f2f45c084d230196dcaf5e9);
+            eventFunctions.Add("Demo_30b2fb78ba854d0986b2329052b9ecc4",Demo_30b2fb78ba854d0986b2329052b9ecc4);
+            eventFunctions.Add("Demo_46a607dd382c49d2a8c319de735e8157",Demo_46a607dd382c49d2a8c319de735e8157);
+            dialogueChecks.Add("Demo_1105168d7cf24844bf025efcc454013f_9c8aec032182475b98a2d7ef96b5887c",Demo_1105168d7cf24844bf025efcc454013f_9c8aec032182475b98a2d7ef96b5887c);
+            dialogueChecks.Add("Demo_1105168d7cf24844bf025efcc454013f_2e82160d18ce4a8b812ab874a3fbb742",Demo_1105168d7cf24844bf025efcc454013f_2e82160d18ce4a8b812ab874a3fbb742);
+            dialogueChecks.Add("Demo_1105168d7cf24844bf025efcc454013f_37d9ac168f6f4307895dd5f569baf054",Demo_1105168d7cf24844bf025efcc454013f_37d9ac168f6f4307895dd5f569baf054);
+            conditionChecks.Add("Demo_fd248e7fbb8b43a0bce1aab11f7ae84f",Demo_fd248e7fbb8b43a0bce1aab11f7ae84f);
+            eventFunctions.Add("Demo_7b28363f37d44e84a6b37b83252ff565",Demo_7b28363f37d44e84a6b37b83252ff565);
+            eventFunctions.Add("Demo_465747a1391f4eefac5d2a1344edd8e5",Demo_465747a1391f4eefac5d2a1344edd8e5);
+            dialogueChecks.Add("Demo_27776fa84a1644d18680558c02f485c3_8c5a554f8fb74b0c88efa338653a0a9a",Demo_27776fa84a1644d18680558c02f485c3_8c5a554f8fb74b0c88efa338653a0a9a);
+            dialogueChecks.Add("Demo_27776fa84a1644d18680558c02f485c3_342e6c8780eb44cb90168651300a2253",Demo_27776fa84a1644d18680558c02f485c3_342e6c8780eb44cb90168651300a2253);
 
         }
 
         // Dialogue Checks //
         // From Node: 1105168d-7cf2-4844-bf02-5efcc454013f //
         // Choice: 70,264 CATS NOW!!! - 9c8aec03-2182-475b-98a2-d7ef96b5887c //
-        public bool New_Dialogue_1105168d7cf24844bf025efcc454013f_9c8aec032182475b98a2d7ef96b5887c()
+        public bool Demo_1105168d7cf24844bf025efcc454013f_9c8aec032182475b98a2d7ef96b5887c()
         {
             return (true);
         }
         // From Node: 1105168d-7cf2-4844-bf02-5efcc454013f //
         // Choice: 2 Cats! - 2e82160d-18ce-4a8b-812a-b874a3fbb742 //
-        public bool New_Dialogue_1105168d7cf24844bf025efcc454013f_2e82160d18ce4a8b812ab874a3fbb742()
+        public bool Demo_1105168d7cf24844bf025efcc454013f_2e82160d18ce4a8b812ab874a3fbb742()
         {
             return (true);
         }
         // From Node: 1105168d-7cf2-4844-bf02-5efcc454013f //
         // Choice: 1 cat - 37d9ac16-8f6f-4307-895d-d5f569baf054 //
-        public bool New_Dialogue_1105168d7cf24844bf025efcc454013f_37d9ac168f6f4307895dd5f569baf054()
+        public bool Demo_1105168d7cf24844bf025efcc454013f_37d9ac168f6f4307895dd5f569baf054()
         {
             return (true);
         }
         // From Node: 27776fa8-4a16-44d1-8680-558c02f485c3 //
         // Choice: YES GIVE ME ALL THE CATS - 8c5a554f-8fb7-4b0c-88ef-a338653a0a9a //
-        public bool New_Dialogue_27776fa84a1644d18680558c02f485c3_8c5a554f8fb74b0c88efa338653a0a9a()
+        public bool Demo_27776fa84a1644d18680558c02f485c3_8c5a554f8fb74b0c88efa338653a0a9a()
         {
             return (cats > 1);
         }
         // From Node: 27776fa8-4a16-44d1-8680-558c02f485c3 //
         // Choice: No thank you! - 342e6c87-80eb-44cb-9016-8651300a2253 //
-        public bool New_Dialogue_27776fa84a1644d18680558c02f485c3_342e6c8780eb44cb90168651300a2253()
+        public bool Demo_27776fa84a1644d18680558c02f485c3_342e6c8780eb44cb90168651300a2253()
         {
             return (cats <= 1);
         }
@@ -83,30 +83,30 @@ string name = "Becky Sue";
 
         // Condition Checks //
         // Condition From Node: fd248e7f-bb8b-43a0-bce1-aab11f7ae84f //
-        public bool New_Dialogue_fd248e7fbb8b43a0bce1aab11f7ae84f() {
+        public bool Demo_fd248e7fbb8b43a0bce1aab11f7ae84f() {
             return (cats > 5);
         }
 
 
         // Event Functions //
         // Event From Node: 538836df-8f2f-45c0-84d2-30196dcaf5e9 //
-        public void New_Dialogue_538836df8f2f45c084d230196dcaf5e9() {
+        public void Demo_538836df8f2f45c084d230196dcaf5e9() {
 cats += 1;
         }
         // Event From Node: 30b2fb78-ba85-4d09-86b2-329052b9ecc4 //
-        public void New_Dialogue_30b2fb78ba854d0986b2329052b9ecc4() {
+        public void Demo_30b2fb78ba854d0986b2329052b9ecc4() {
 cats += 2;
         }
         // Event From Node: 46a607dd-382c-49d2-a8c3-19de735e8157 //
-        public void New_Dialogue_46a607dd382c49d2a8c319de735e8157() {
+        public void Demo_46a607dd382c49d2a8c319de735e8157() {
 cats += 70264;
         }
         // Event From Node: 7b28363f-37d4-4e84-a6b3-7b83252ff565 //
-        public void New_Dialogue_7b28363f37d44e84a6b37b83252ff565() {
+        public void Demo_7b28363f37d44e84a6b37b83252ff565() {
 cats -= 70000;
         }
         // Event From Node: 465747a1-391f-4eef-ac5d-2a1344edd8e5 //
-        public void New_Dialogue_465747a1391f4eefac5d2a1344edd8e5() {
+        public void Demo_465747a1391f4eefac5d2a1344edd8e5() {
 cats++;
         }
 
