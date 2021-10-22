31fc1bb9f38cd40abab640135de846d1c268e832
sim2kid <sim2kid@users.noreply.github.com>
2021-10-22 09:55:26 -0500

    start creature generation script

---
+++ Assets/Scripts/Genetics/CreatureGeneration.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Genetics
+{
+    public static class CreatureGeneration 
+    {
+        public static GameObject CreateRandomCreature() 
+        {
+            return null;
+        }
+
+        public static GameObject CreateCreature(DNA dna) 
+        {
+            return null;
+        }
+    }
+}
