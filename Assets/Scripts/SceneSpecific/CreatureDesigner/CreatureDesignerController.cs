c2099bdc0d02d8ee0a112a6d7fdbfc6d148c072e
sim2kid <sim2kid@users.noreply.github.com>
2021-10-28 17:48:30 -0500

    reworked the interface for part swaping

--- Assets/Scripts/SceneSpecific/CreatureDesigner/CreatureDesignerController.cs
+++ Assets/Scripts/SceneSpecific/CreatureDesigner/CreatureDesignerController.cs
@@ -7,21 +7,13 @@ using TMPro;
 public class CreatureDesignerController : MonoBehaviour
 {
     GeneticRepository genePool;
-    public TextMeshProUGUI accessoriesIndex;
-    public TextMeshProUGUI bodiesIndex;
-    public TextMeshProUGUI earsIndex;
-    public TextMeshProUGUI hatsIndex;
-    public TextMeshProUGUI headsIndex;
-    public TextMeshProUGUI hornsIndex;
-    public TextMeshProUGUI legsIndex;
-    public TextMeshProUGUI masksIndex;
-    public TextMeshProUGUI tailsIndex;
-    private int[] indexs;
+    public TextMeshProUGUI partType;
+    public TextMeshProUGUI bodyPart;
+    public TextMeshProUGUI pattern;
 
     // Start is called before the first frame update
     void Start()
     {
-        indexs = new int[6];
         genePool = Utility.Toolbox.Instance.GenePool;
     }
 
