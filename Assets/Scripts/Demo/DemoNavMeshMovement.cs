820c7bac0f8d9d328a05bc7ba28dca7705b8ae9b
sim2kid <sim2kid@users.noreply.github.com>
2021-10-29 10:22:20 -0500

    Refactored the NavMesh Script

--- Assets/Scripts/Demo/DemoNavMeshMovement.cs
+++ Assets/Scripts/Demo/DemoNavMeshMovement.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
+[DisallowMultipleComponent]
 public class DemoNavMeshMovement : MonoBehaviour
 {
     [Tooltip("Point in 3D space the NavMesh agent will attempt to move to.")]
     [SerializeField] Vector3 destinationVector3;
     [Tooltip("Game object in the scene the NavMesh agent will attempt to move to.")]
     [SerializeField] Transform destinationTransform;
-    [Tooltip("The NavMesh Agent component attached to this game object.")]
+
     private NavMeshAgent navMeshAgent;
     // Start is called before the first frame update
     void Start()
     {
         GetNavMeshAgent();
         //MoveToTransform(destinationTransform);
-        MoveToVector3(destinationVector3);
+        MoveTo(destinationVector3);
     }
+
     /// <summary>
     /// Find and assign the NavMesh Agent component attached to this game object.
     /// </summary>
@@ -25,11 +28,12 @@ public class DemoNavMeshMovement : MonoBehaviour
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
+
     /// <summary>
     /// Attempt to move the NavMesh agent to a specified point in 3D space.
     /// </summary>
     /// <param name="_destination">Point in 3D space the NavMesh agent will attempt to move to.</param>
-    void MoveToVector3(Vector3 _destination)
+    void MoveTo(Vector3 _destination)
     {
         navMeshAgent.destination = _destination;
     }
@@ -37,8 +41,8 @@ public class DemoNavMeshMovement : MonoBehaviour
     /// Attempt to move the NavMesh agent to a specified game object in the scene.
     /// </summary>
     /// <param name="_destination">Game object in the scene the NavMesh agent will attempt to move to.</param>
-    void MoveToTransform(Transform _destination)
+    void MoveTo(Transform _destination)
     {
-        navMeshAgent.destination = _destination.position;
+        MoveTo(_destination.position);
     }
 }
