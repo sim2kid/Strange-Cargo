515c88f100f886aa7937d1b9988c74f404ad8d75
sim2kid <sim2kid@users.noreply.github.com>
2021-10-28 22:17:41 -0500

    Add Interaction Framework
    resolves #16

--- Assets/Scripts/Interaction/IInteractable.cs
+++ Assets/Scripts/Interaction/IInteractable.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 namespace Interaction
 {
     public interface IInteractable
-    { 
-        
+    {
+        public ClickState ClickState { get; }
+
+        public void Enter();
+        public void Hover();
+        public void Exit();
+
+        public void Down();
+        public void Hold();
+        public void Up();
     }
 }
