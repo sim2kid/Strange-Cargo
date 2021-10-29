b073e466092b047641484fbc12d96d6b8567f20a
sim2kid <sim2kid@users.noreply.github.com>
2021-10-29 11:50:08 -0500

    played wtih time controller

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class TimeController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// Seconds Elapsed
+    /// </summary>
+    private float _time;
+
+    [SerializeField]
+    private float CurrentTime;
+
+    [Tooltip("The real life minutes per Day/Night cycle")]
+    public float MinutesInADay = 13.25f;
+
+    private void OnEnable()
+    {
+        Utility.Toolbox.Instance.TimeController = this;
+    }
+
     void Start()
     {
-        
+        _time = 0;    
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Gives seconds elapsed
+        _time += Time.fixedDeltaTime;
+
+        if (_time > MinutesInADay * 60) 
+        {
+            _time -= MinutesInADay * 60;
+        }
+
+        Debug.Log(GetTime());
+
+        CurrentTime = GetTime();
+    }
+
+    /// <summary>
+    /// Returns the time between 0-24 representing the hours of a day
+    /// </summary>
+    /// <returns></returns>
+    public float GetTime() 
+    {
+        return (_time/(MinutesInADay*60)) * 24;
     }
 }
