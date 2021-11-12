8c0f2fbbec858135303b4324f38de84e37c037a7
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2021-11-11 23:07:01 -0600

    sets up basic player animation control logic

--- Assets/Scripts/PlayerMovement/PlayerAnimationControllerDemo.cs
+++ Assets/Scripts/PlayerMovement/PlayerAnimationControllerDemo.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
 public class PlayerAnimationControllerDemo : MonoBehaviour
 {
+    private CharacterController characterController;
+    private Vector2 groundVelocity;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        groundVelocity = new Vector2(characterController.velocity.x, characterController.velocity.z);
+        animator.SetFloat("GroundSpeed", groundVelocity.magnitude);
+        animator.SetFloat("VerticalSpeed", characterController.velocity.y);
     }
 }
