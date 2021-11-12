fbab1d6d64d57f81d983ed35728f0cab86a47648
sim2kid <sim2kid@users.noreply.github.com>
2021-11-12 02:36:37 -0600

    Added some demo code

--- Assets/Scripts/Demo/HardCodedComehere.cs
+++ Assets/Scripts/Demo/HardCodedComehere.cs
@@ -10,11 +10,19 @@ public class HardCodedComehere : MonoBehaviour
 {
     [SerializeField]
     Transform location;
+    [SerializeField]
+    FoodBowl bowl;
 
     public void ComeHere() 
     {
         if (location == null)
             return;
         Utility.Toolbox.Instance.CreatureList.First().AddTask(new GoHere(location, 20));
+    }
+    public void Eat()
+    {
+        if (location == null)
+            return;
+        Utility.Toolbox.Instance.CreatureList.First().AddTask(new Eat(bowl));
     }
 }
