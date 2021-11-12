6f18a4b69ff9afdc9e9435c1834133d493f2e696
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2021-11-11 23:16:49 -0600

    adds basic player animation framework

--- Assets/Scripts/PlayerMovement/PlayerAnimationControllerDemo.cs
+++ Assets/Scripts/PlayerMovement/PlayerAnimationControllerDemo.cs
@@ -1,19 +1,23 @@
+using PlayerController;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(MovementController))]
 public class PlayerAnimationControllerDemo : MonoBehaviour
 {
     private CharacterController characterController;
     private Vector2 groundVelocity;
     private Animator animator;
+    private MovementController movementController;
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+        movementController = GetComponent<MovementController>();
     }
 
     // Update is called once per frame
@@ -21,6 +25,9 @@ public class PlayerAnimationControllerDemo : MonoBehaviour
     {
         groundVelocity = new Vector2(characterController.velocity.x, characterController.velocity.z);
         animator.SetFloat("GroundSpeed", groundVelocity.magnitude);
-        animator.SetFloat("VerticalSpeed", characterController.velocity.y);
+        if (!movementController.isOnGround)
+        {
+            animator.SetFloat("VerticalSpeed", characterController.velocity.y);
+        }
     }
 }
