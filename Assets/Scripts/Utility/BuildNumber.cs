ac7274e1d5a451fc004b0b2a32ba04cc2f73a561
sim2kid <sim2kid@users.noreply.github.com>
2021-11-20 06:28:45 -0600

    Added build number indicator on GUI

---
+++ Assets/Scripts/Utility/BuildNumber.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Utility
+{
+    [RequireComponent(typeof(TMPro.TextMeshProUGUI))]
+    public class BuildNumber : MonoBehaviour
+    {
+        TMPro.TextMeshProUGUI tmp;
+        private void Start()
+        {
+            tmp = GetComponent<TMPro.TextMeshProUGUI>();
+            tmp.text = tmp.text.Replace("#.#.#", Application.version);
+            Console.Debug(tmp.text);
+        }
+    }
+}
