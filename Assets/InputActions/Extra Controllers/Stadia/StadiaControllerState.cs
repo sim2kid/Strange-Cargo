e805e5fac2d53db8716615994ecb530ee3291ae7
sim2kid <sim2kid@users.noreply.github.com>
2021-12-11 12:04:13 -0600

    Updated controller support

--- Assets/InputActions/Extra Controllers/Stadia/StadiaControllerState.cs
+++ Assets/InputActions/Extra Controllers/Stadia/StadiaControllerState.cs
@@ -1,20 +1,18 @@
-// We receive data as raw HID input reports. This struct
-// describes the raw binary format of such a report.
-using System.Runtime.InteropServices;
 using UnityEngine.InputSystem.Layouts;
+using System.Runtime.InteropServices;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.InputSystem.Utilities;
 
-[StructLayout(LayoutKind.Explicit, Size = 32)]
-struct StadiaGamepadInputReport : IInputStateTypeInfo
+[StructLayout(LayoutKind.Explicit, Size = 10)]
+struct StadiaControllerState : IInputStateTypeInfo
 {
     // Because all HID input reports are tagged with the 'HID ' FourCC,
     // this is the format we need to use for this state struct.
     public FourCC format => new FourCC('H', 'I', 'D');
 
     // HID input reports can start with an 8-bit report ID. It depends on the device
-    // whether this is present or not. On the PS4 DualShock controller, it is
-    // present. We don't really need to add the field, but let's do so for the sake of
+    // whether this is present or not. I don't believe this is present on a Stadia
+    // Controller. We don't really need to add the field, but let's do so for the sake of
     // completeness. This can also help with debugging.
     [FieldOffset(0)] public byte reportId;
 
@@ -26,6 +24,7 @@ struct StadiaGamepadInputReport : IInputStateTypeInfo
     // Given that our layout is based on Gamepad, almost all the controls here are
     // inherited from Gamepad, and we just modify settings on them.
 
+    // The DPad inputs are the same as a PS DuelShock controller.
     [InputControl(name = "dpad", format = "BIT", layout = "Dpad", sizeInBits = 4, defaultState = 8)]
     [InputControl(name = "dpad/up", format = "BIT", layout = "DiscreteButton", parameters = "minValue=7,maxValue=1,nullValue=8,wrapAtValue=7", bit = 0, sizeInBits = 4)]
     [InputControl(name = "dpad/right", format = "BIT", layout = "DiscreteButton", parameters = "minValue=1,maxValue=3", bit = 0, sizeInBits = 4)]
@@ -34,32 +33,39 @@ struct StadiaGamepadInputReport : IInputStateTypeInfo
     [InputControl(name = "dpad/x", format = "BIT", layout = "DpadAxis")]
     [InputControl(name = "dpad/y", format = "BIT", layout = "DpadAxis")]
 
-
-
+    // The next 4 bits are empty?
 
     [FieldOffset(1)] public byte buttons1;
 
 
-    [InputControl(name = "captureButton", layout = "DiscreteButton", displayName = "Capture", offset = 2, bit = 0)]
-    [InputControl(name = "googleButton", layout = "DiscreteButton", displayName = "Assistant", offset = 2, bit = 1)]
+
+    // These are non-regular to gampads and thus are optional
+    [InputControl(name = "captureButton", layout = "Button", displayName = "Capture", offset = 2, bit = 0)]
+    [InputControl(name = "googleButton", layout = "Button", displayName = "Assistant", offset = 2, bit = 1)]
     [InputControl(name = "leftTriggerButton", layout = "Button", bit = 2)]
     [InputControl(name = "rightTriggerButton", layout = "Button", bit = 3)]
+    // The Stadia Button
+    [InputControl(name = "systemButton", displayName = "Home", layout = "Button", bit = 4)]
+
 
     [InputControl(name = "start", displayName = "Menu", offset = 2, bit = 5)]
-    [InputControl(name = "select", displayName = "More", offset = 2, bit = 6)]
+    [InputControl(name = "select", displayName = "Options", offset = 2, bit = 6)]
     [InputControl(name = "rightStickPress", bit = 7)]
-    
     [FieldOffset(2)] public byte buttons2;
 
-    [InputControl(name = "leftShoulder", bit = 2)]
-    [InputControl(name = "rightShoulder", bit = 1)]
     [InputControl(name = "leftStickPress", bit = 0)]
-    [InputControl(name = "buttonWest", displayName = "X", bit = 4)]
-    [InputControl(name = "buttonSouth", displayName = "A", bit = 6)]
-    [InputControl(name = "buttonEast", displayName = "B", bit = 5)]
+    [InputControl(name = "rightShoulder", bit = 1)]
+    [InputControl(name = "leftShoulder", bit = 2)]
     [InputControl(name = "buttonNorth", displayName = "Y", bit = 3)]
+    [InputControl(name = "buttonWest", displayName = "X", bit = 4)]
+    [InputControl(name = "buttonEast", displayName = "B", bit = 5)]
+    [InputControl(name = "buttonSouth", displayName = "A", bit = 6)]
+    // Unused Bit?
     [FieldOffset(3)] public byte buttons3;
 
+
+
+    // Thakes up byte 4
     [InputControl(name = "leftStick", layout = "Stick", format = "VC2B")]
     [InputControl(name = "leftStick/x", format = "BYTE",
         parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5")]
@@ -68,6 +74,7 @@ struct StadiaGamepadInputReport : IInputStateTypeInfo
     [InputControl(name = "leftStick/right", format = "BYTE",
         parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5,clamp,clampMin=0.5,clampMax=1")]
 
+    // Thakes up byte 5
     [InputControl(name = "leftStick/y", offset = 1, format = "BYTE",
         parameters = "invert,normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5")]
     [InputControl(name = "leftStick/up", offset = 1, format = "BYTE",
@@ -77,21 +84,30 @@ struct StadiaGamepadInputReport : IInputStateTypeInfo
     [FieldOffset(4)] public byte leftStickX;
     [FieldOffset(5)] public byte leftStickY;
 
+
+
+    // Thakes up byte 6
     [InputControl(name = "rightStick", layout = "Stick", format = "VC2B")]
     [InputControl(name = "rightStick/x", format = "BYTE", parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5")]
     [InputControl(name = "rightStick/left", format = "BYTE", parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5,clamp,clampMin=0,clampMax=0.5,invert")]
     [InputControl(name = "rightStick/right", format = "BYTE", parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5,clamp,clampMin=0.5,clampMax=1")]
 
+    // Thakes up byte 7
     [InputControl(name = "rightStick/y", offset = 1, format = "BYTE", parameters = "invert,normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5")]
     [InputControl(name = "rightStick/up", offset = 1, format = "BYTE", parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5,clamp,clampMin=0,clampMax=0.5,invert")]
     [InputControl(name = "rightStick/down", offset = 1, format = "BYTE", parameters = "normalize,normalizeMin=0,normalizeMax=1,normalizeZero=0.5,clamp,clampMin=0.5,clampMax=1,invert=false")]
     [FieldOffset(6)] public byte rightStickX;
     [FieldOffset(7)] public byte rightStickY;
 
-  
+
+
+    // Thakes up byte 8
     [InputControl(name = "leftTrigger", format = "BYTE")]
     [FieldOffset(8)] public byte leftTrigger;
 
+
+
+    // Thakes up byte 9
     [InputControl(name = "rightTrigger", format = "BYTE")]
     [FieldOffset(9)] public byte rightTrigger;
 }
