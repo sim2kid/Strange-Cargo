7a368e5499855540253cc4f0166ba5a1b96db0dc
sim2kid <sim2kid@users.noreply.github.com>
2022-01-12 10:29:50 -0600

    Files can now be saved and loaded from disk

--- Assets/Scripts/PersistentData/SaveManager.cs
+++ Assets/Scripts/PersistentData/SaveManager.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using PersistentData.Component;
 using PersistentData.Saving;
+using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 
 namespace PersistentData
 {
     public class SaveManager : MonoBehaviour
     {
-        string s;
+        static string SaveLocation;
+        Save s;
+        private void Awake()
+        {
+            SaveLocation = System.IO.Path.Combine(Application.persistentDataPath, "Saves");
+        }
+
         public Save MakeSave() 
         {
             PrefabSaveable[] objectsToSave = FindObjectsOfType<PrefabSaveable>();
@@ -28,11 +37,75 @@ namespace PersistentData
             return save;
         }
 
-        public void SaveToDisk() 
+        public void DemoSave() 
         {
             Save save = MakeSave();
-            s = Newtonsoft.Json.JsonConvert.SerializeObject(save);
+            SaveToDisk(save);
+        }
+
+        public void DemoLoad() 
+        {
+            s = LoadFromDisk("Save Name");
             return;
+        }
+
+        private Save LoadFromDisk(string saveName) 
+        {
+            string loc = SanitizePath(Path.Combine(SaveLocation, RemoveIllegalCharacters(saveName) + ".save"));
+
+            if (!Directory.Exists(SaveLocation))
+                Directory.CreateDirectory(SaveLocation);
+            if (!File.Exists(loc)) 
+            {
+                Console.LogWarning($"Could not find save '{saveName}'. Is it spelled correctly?");
+                return new Save() { SaveTime = -1 };
+            }
+
+            byte[] saveBytes = File.ReadAllBytes(loc);
+            string saveJson = DecryptBytes(saveBytes);
+
+            Save save = JsonConvert.DeserializeObject<Save>(saveJson);
+            return save;
+        }
+
+        private void SaveToDisk(Save save) 
+        {
+            string loc = SanitizePath(Path.Combine(SaveLocation, RemoveIllegalCharacters(save.SaveName) + ".save"));
+            string saveJson = JsonConvert.SerializeObject(save);
+
+            if(!Directory.Exists(SaveLocation))
+                Directory.CreateDirectory(SaveLocation);
+
+            File.WriteAllBytes(loc, EncryptString(saveJson));
+
+            return;
+        }
+
+        private static byte[] EncryptString(string str) 
+        {
+            return System.Text.Encoding.UTF8.GetBytes(str);
+        }
+
+        private static string DecryptBytes(byte[] bytes) 
+        {
+            return System.Text.Encoding.UTF8.GetString(bytes);
+        }
+
+        private static bool HasIlligalCharacters(string s) 
+        {
+            return !s.Equals(RemoveIllegalCharacters(s));
+        }
+
+        private static string RemoveIllegalCharacters(string s) 
+        {
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(s, "");
+        }
+
+        private static string SanitizePath(string s)
+        {
+            return s.Replace('/', '\\');
         }
     }
 }
