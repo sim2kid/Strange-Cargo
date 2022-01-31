fb922c1573eedbf9b245fcde7c5623a026358de6
sim2kid <sim2kid@users.noreply.github.com>
2022-01-31 13:09:00 -0600

    Added hologram shader

---
+++ Assets/Scripts/Placement/PlacementController.cs
@@ -0,0 +1,25 @@
+using Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlacementController : MonoBehaviour
+{
+    private PlayerController player;
+    private Transform headTransform;
+    [SerializeField]
+    private Shader hologramShader;
+
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+        headTransform = player.Eyes.transform;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
