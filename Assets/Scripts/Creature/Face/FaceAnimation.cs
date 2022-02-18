be7582fcaec5bd67085432473cd656c67fceaa18
Max Garay <382535@colum.edu>
2022-02-18 10:00:34 -0600

    add basic faceanimation class

---
+++ Assets/Scripts/Creature/Face/FaceAnimation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Creature.Face
+{
+    public class FaceAnimation
+    {
+        public string name;
+        public float timing;
+    }
+}
