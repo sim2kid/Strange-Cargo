742f2f1a25ca754b704eed34e77155cc996d4708
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2022-03-02 11:52:36 -0600

    update creaturedesigncontroller (not finished)

--- Assets/Scripts/SceneSpecific/CreatureDesigner/CreatureDesignerController.cs
+++ Assets/Scripts/SceneSpecific/CreatureDesigner/CreatureDesignerController.cs
@@ -10,21 +10,25 @@ public class CreatureDesignerController : MonoBehaviour
     public TextMeshProUGUI partType;
     public TextMeshProUGUI bodyPart;
     public TextMeshProUGUI pattern;
+    GameObject sampleCreature;
+    DNA sampleCreatureDNA;
 
     // Start is called before the first frame update
     void Start()
     {
         genePool = Utility.Toolbox.Instance.GenePool;
+        sampleCreature = CreatureGeneration.CreateCreature();
+        sampleCreatureDNA = sampleCreature.GetComponent<Creature.CreatureController>().dna;
     }
 
     public void Build() 
     {
-        
+
     }
 
     public void Next(string bodypart) 
     {
-    
+
     }
 
     public void Last(string bodypart)
