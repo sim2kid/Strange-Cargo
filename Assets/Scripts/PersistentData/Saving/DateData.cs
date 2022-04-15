925bfd2a22c9a2221f5904118cb534dc97d4b668
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2022-04-14 22:15:06 -0500

    add datedata

---
+++ Assets/Scripts/PersistentData/Saving/DateData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace PersistentData.Saving
+{
+    [System.Serializable]
+    public struct DateData : ISaveData
+    {
+        [JsonIgnore]
+        public string _guid;
+        public string GUID { get => _guid; set => _guid = value; }
+        public string DataType => "Date";
+        [HideInInspector]
+        public Vector2 Date;
+    }
+}
