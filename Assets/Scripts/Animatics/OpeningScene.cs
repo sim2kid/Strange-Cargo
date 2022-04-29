624c06ad291ad28b6dd746ce1dccf60d22c5058f
sim2kid <sim2kid@users.noreply.github.com>
2022-04-29 04:08:51 -0500

    Add skip

--- Assets/Scripts/Animatics/OpeningScene.cs
+++ Assets/Scripts/Animatics/OpeningScene.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UI;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.Video;
 
 namespace Animatics
@@ -11,6 +14,8 @@ namespace Animatics
     {
         public UnityEvent OnFinish = new UnityEvent();
         public VideoClip video;
+        bool isPlaying;
+        bool anyKey;
 
         void Start()
         {
@@ -20,12 +25,43 @@ namespace Animatics
             Utility.Toolbox.Instance.Pause.SetPause(true);
             bgm.PauseMusic();
             // play animatic
+            isPlaying = true;
             GameObject.FindObjectOfType<UIManager>().OpenVideo(video)?.AddListener(() => 
             {
+                isPlaying = false;
                 Utility.Toolbox.Instance.Pause.SetPause(false);
                 bgm.UnpauseMusic();
                 OnFinish.Invoke();
             });
+
+            InputSystem.onEvent +=
+            (eventPtr, device) =>
+            {
+                if (!eventPtr.IsA<StateEvent>() && !eventPtr.IsA<DeltaStateEvent>())
+                    return;
+                var controls = device.allControls;
+                var buttonPressPoint = InputSystem.settings.defaultButtonPressPoint;
+                for (var i = 0; i < controls.Count; ++i)
+                {
+                    var control = controls[i] as ButtonControl;
+                    if (control == null || control.synthetic || control.noisy)
+                        continue;
+                    if (control.ReadValueFromEvent(eventPtr, out var value) && value >= buttonPressPoint)
+                    {
+                        anyKey = true;
+                        break;
+                    }
+                }
+            };
+        }
+
+        private void Update()
+        {
+            if (isPlaying && anyKey) 
+            {
+                isPlaying = false;
+                GameObject.FindObjectOfType<PauseMenu>().StopVideo();
+            }
         }
     }
 }
